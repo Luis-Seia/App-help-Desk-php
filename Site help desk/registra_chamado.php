14aaa188c83ca9c7065ba3bc5e19c7202a93f3f2
Luis Seia <luis.seiax@gmail.com>
2022-09-01 18:16:31 +0200

    novo comentario

--- Site help desk/registra_chamado.php
+++ Site help desk/registra_chamado.php
@@ -1,14 +1,11 @@
 <?php 
 $arquivo = fopen('arquivo.txt', 'a');
-
 $texto = 'Titulo: '. $_POST['titulo'] .'<br> Categoria: ' .$_POST['categoria'] .'<br> Descricao: ' .$_POST['descricao'] .PHP_EOL; 
-
 echo $texto;
 
 
 fwrite($arquivo, $texto);
 fclose($arquivo);
-
 header('Location: abrir_chamado.php');
 
 ?>
--- Site help desk/valida_login.php
+++ Site help desk/valida_login.php
@@ -6,14 +6,12 @@ $usuarios_app = array(
     array('email' =>'luis.seiax@gmail.com', 'senha' => '210502') ,  
     array('email' =>'luis.seia.com', 'senha' => '251102')
 );
-
 $usuarioAutenticado = false;
 foreach($usuarios_app as $user){
     if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
         $usuarioAutenticado = true;
     }  
 } 
-
 if($usuarioAutenticado){
     $_SESSION['autenticado'] = 'sim';
     header('Location:home.php');
@@ -21,5 +19,6 @@ if($usuarioAutenticado){
     header('Location:index.php?login=erro'); // funcao para redicionamento
     $_SESSION['autenticado'] = 'nao';
 }
-
+// lkdvaklngorjgoewn
+//etyy6ru6545
 ?>
